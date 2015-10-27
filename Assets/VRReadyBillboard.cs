d864924a4feb8317dc0fb141c3fb1ce0f378f91c
Frederico Zveiter <frederico.zveiter@gmail.com>
2015-10-26 19:11:04 -0700

    Pushing test project.

---
+++ Assets/VRReadyBillboard.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class VRReadyBillboard : MonoBehaviour
+{
+    private void Update()
+    {
+        transform.rotation = Quaternion.LookRotation(transform.position - Camera.main.transform.position, Vector3.up);
+    }
+}
